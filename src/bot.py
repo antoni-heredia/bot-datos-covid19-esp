a99f0f0e59c87bd4cf430700c5fc9d46b6fbc336
Antonio Jesus Heredia Castillo <a.heredia.castillo@gmail.com>
2020-03-22 16:51:27 +0100

    cambiado a webhook

--- src/bot.py
+++ src/bot.py
@@ -1,78 +1,39 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-import logging,csv
-import urllib.request
+# bot.py
+import requests  
 import os
+from flask import Flask, request
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger('Covid19-esp')
+# Add your telegram token as environment variable
+if not 'HEROKU' in os.environ:
+    from config.auth import token
+    BOT_URL = f'https://api.telegram.org/bot{token}/'
+else:
+    #Si esta desplegado en Heroku
+    BOT_URL = f'https://api.telegram.org/bot{os.environ["TOKEN"]}/'
 
-def start(bot, update):
-    logger.info('He recibido un comando start. Usuario: '+ str(update.message.chat_id))
-    #Mando la información referente al comando
-    mensaje ="La información que ofrece este bot es la publicada por el Ministerio de Sanidad Español. La información puede encontrarse en la web: covid19.isciii.es .\n"
-    mensaje += "*Lista de comandos:*\n"
-    mensaje += "/start - Inicia el bot y ofrece información inicial\n"
-    mensaje += "/esp - Ofrece los datos generales de todo el estado\n"
-    mensaje += "/comunidades - Ofrece los datos por comunidades autónomas\n"
-    #Se envia al chat_id
-    #el texto es el mensaje
-    #Y como estoy usando markdown indico que lo parse de ese modo
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=mensaje,
-        parse_mode = "Markdown"
-    )
-def esp(bot,update):
-    logger.info('He recibido un comando espania. Usuario: '+ str(update.message.chat_id))
-    mensaje = ""
-    with open('datos/data.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            mensaje += "*Fecha:* " + row['Fecha'] + "\n"
-            mensaje += "*Hora:* " + row['Hora'] + "\n"
-            mensaje += "*Casos Totales:* " + row['Casos'] + "\n"
-            mensaje += "*Defunciones:* " + row['Defunciones'] + "\n"
-            mensaje += "*Recuperados:* " + row['Recuperados'] + "\n"
 
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=mensaje,
-        parse_mode = "Markdown"
-    )
+app = Flask(__name__)
 
-def comunidades(bot,update):
-    logger.info('He recibido un comando comunidades. Usuario: '+ str(update.message.chat_id))
-    mensaje = ""
-    with open('datos/ccaa.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            mensaje += "*"+row["﻿CCAA"]+":* "+row["Nº Casos"] + "\n"
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=mensaje,
-        parse_mode = "Markdown"
-    )
-if __name__ == '__main__':
-    #Si se ejecuta en local
-    if not 'HEROKU' in os.environ:
-        from config.auth import token
-        updater = Updater(token)
-    else:
-        #Si esta desplegado en Heroku
-        updater = Updater(os.environ['TOKEN'])
 
-    dispatcher = updater.dispatcher
+@app.route('/', methods=['POST'])
+def main():  
+    data = request.json
 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    print(data)  # Comment to hide what Telegram is sending you
+    chat_id = data['message']['chat']['id']
+    message = data['message']['text']
 
-    espania_handler = CommandHandler('esp', esp)
-    dispatcher.add_handler(espania_handler)
+    json_data = {
+        "chat_id": chat_id,
+        "text": message,
+    }
 
-    comunidades_handler = CommandHandler('comunidades', comunidades)
-    dispatcher.add_handler(comunidades_handler)
+    message_url = BOT_URL + 'sendMessage'
+    requests.post(message_url, json=json_data)
 
-    updater.start_polling()
-    updater.idle()
+    return ''
 
+
+if __name__ == '__main__':  
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
---
+++ src/bot_telegram.ext.py
@@ -0,0 +1,78 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import logging,csv
+import urllib.request
+import os
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger('Covid19-esp')
+
+def start(bot, update):
+    logger.info('He recibido un comando start. Usuario: '+ str(update.message.chat_id))
+    #Mando la información referente al comando
+    mensaje ="La información que ofrece este bot es la publicada por el Ministerio de Sanidad Español. La información puede encontrarse en la web: covid19.isciii.es .\n"
+    mensaje += "*Lista de comandos:*\n"
+    mensaje += "/start - Inicia el bot y ofrece información inicial\n"
+    mensaje += "/esp - Ofrece los datos generales de todo el estado\n"
+    mensaje += "/comunidades - Ofrece los datos por comunidades autónomas\n"
+    #Se envia al chat_id
+    #el texto es el mensaje
+    #Y como estoy usando markdown indico que lo parse de ese modo
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=mensaje,
+        parse_mode = "Markdown"
+    )
+def esp(bot,update):
+    logger.info('He recibido un comando espania. Usuario: '+ str(update.message.chat_id))
+    mensaje = ""
+    with open('datos/data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            mensaje += "*Fecha:* " + row['Fecha'] + "\n"
+            mensaje += "*Hora:* " + row['Hora'] + "\n"
+            mensaje += "*Casos Totales:* " + row['Casos'] + "\n"
+            mensaje += "*Defunciones:* " + row['Defunciones'] + "\n"
+            mensaje += "*Recuperados:* " + row['Recuperados'] + "\n"
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=mensaje,
+        parse_mode = "Markdown"
+    )
+
+def comunidades(bot,update):
+    logger.info('He recibido un comando comunidades. Usuario: '+ str(update.message.chat_id))
+    mensaje = ""
+    with open('datos/ccaa.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            mensaje += "*"+row["﻿CCAA"]+":* "+row["Nº Casos"] + "\n"
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=mensaje,
+        parse_mode = "Markdown"
+    )
+if __name__ == '__main__':
+    #Si se ejecuta en local
+    if not 'HEROKU' in os.environ:
+        from config.auth import token
+        updater = Updater(token)
+    else:
+        #Si esta desplegado en Heroku
+        updater = Updater(os.environ['TOKEN'])
+
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    espania_handler = CommandHandler('esp', esp)
+    dispatcher.add_handler(espania_handler)
+
+    comunidades_handler = CommandHandler('comunidades', comunidades)
+    dispatcher.add_handler(comunidades_handler)
+
+    updater.start_polling()
+    updater.idle()
+
